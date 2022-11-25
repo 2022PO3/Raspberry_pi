1ff2c77c43f28be03e4d393689ff3f15aedc7e31
VanheesRik <rik.vanhees@student.kuleuven.be>
2022-11-25 16:19:19 +0100

    combined functions into one

--- led_controll.py
+++ led_controll.py
@@ -8,206 +8,58 @@ parking4_detected = True
 parking5_detected = True
 parking6_detected = True
 
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 # define pins
-parking1green = 1
-parking1red = 2
-parking2green = 3
-parking2red = 4
-parking3green = 5
-parking3red = 6
-parking4green = 7
-parking4red = 8
-parking5green = 9
-parking5red = 10
-parking6green = 11
-parking6red = 12
+parking1 = 1
+parking2 = 2
+parking3 = 3
+parking4 = 4
+parking5 = 5
+parking6 = 6
+
+parkings_detected = [(parking1_detected, parking1),
+                     (parking2_detected, parking2),
+                     (parking3_detected, parking3),
+                     (parking4_detected, parking4),
+                     (parking5_detected, parking5),
+                     (parking6_detected, parking6)]
 
 # setup
-GPIO.setup(parking1green, GPIO.OUT)
-GPIO.setup(parking1red, GPIO.OUT)
-GPIO.setup(parking2green, GPIO.OUT)
-GPIO.setup(parking2red, GPIO.OUT)
-GPIO.setup(parking3green, GPIO.OUT)
-GPIO.setup(parking3red, GPIO.OUT)
-GPIO.setup(parking4green, GPIO.OUT)
-GPIO.setup(parking4red, GPIO.OUT)
-GPIO.setup(parking5green, GPIO.OUT)
-GPIO.setup(parking5red, GPIO.OUT)
-GPIO.setup(parking6green, GPIO.OUT)
-GPIO.setup(parking6red, GPIO.OUT)
+GPIO.setup(parking1, GPIO.OUT)
+GPIO.setup(parking2, GPIO.OUT)
+GPIO.setup(parking3, GPIO.OUT)
+GPIO.setup(parking4, GPIO.OUT)
+GPIO.setup(parking5, GPIO.OUT)
+GPIO.setup(parking6, GPIO.OUT)
 
 
 # functions turn on green leds
-def turn_on_parking1green():
-    GPIO.output(parking1green, GPIO.HIGH)
-
-
-def turn_on_parking2green():
-    GPIO.output(parking2green, GPIO.HIGH)
-
-
-def turn_on_parking3green():
-    GPIO.output(parking3green, GPIO.HIGH)
-
-
-def turn_on_parking4green():
-    GPIO.output(parking4green, GPIO.HIGH)
-
-
-def turn_on_parking5green():
-    GPIO.output(parking5green, GPIO.HIGH)
-
-
-def turn_on_parking6green():
-    GPIO.output(parking6green, GPIO.HIGH)
-
+def turn_on_green(pin_no: int) -> None:
+    GPIO.output(pin_no, GPIO.HIGH)
 
 # turn on red leds
-def turn_on_parking1red():
-    GPIO.output(parking1red, GPIO.HIGH)
-
-
-def turn_on_parking2red():
-    GPIO.output(parking2red, GPIO.HIGH)
-
-
-def turn_on_parking3red():
-    GPIO.output(parking3red, GPIO.HIGH)
-
-
-def turn_on_parking4red():
-    GPIO.output(parking4red, GPIO.HIGH)
-
-
-def turn_on_parking5red():
-    GPIO.output(parking5red, GPIO.HIGH)
-
-
-def turn_on_parking6red():
-    GPIO.output(parking6red, GPIO.HIGH)
-
+def turn_on_red(pin_no: int) -> None:
+    GPIO.output(pin_no, GPIO.HIGH)
 
 # turn off green leds
-def turn_off_parking1green():
-    GPIO.output(parking1green, GPIO.LOW)
+def turn_off_green(pin_no: int) -> None:
+    GPIO.output(pin_no, GPIO.LOW)
+
+def turn_off_red(pin_no: int) -> None:
+    GPIO.output(pin_no, GPIO.LOW)
 
 
-def turn_off_parking2green():
-    GPIO.output(parking2green, GPIO.LOW)
+for parking in parkings_detected:
+    if parking[1]:
+        turn_on_red(parking[2])
+        turn_off_green(parking[2])
+        time.sleep(0.5)
+    else:
+        turn_off_red(parking[2])
+        turn_on_green(parking[2])
+        time.sleep(0.5)
 
 
-def turn_off_parking3green():
-    GPIO.output(parking3green, GPIO.LOW)
-
-
-def turn_off_parking4green():
-    GPIO.output(parking4green, GPIO.LOW)
-
-
-def turn_off_parking5green():
-    GPIO.output(parking5green, GPIO.LOW)
-
-
-def turn_off_parking6green():
-    GPIO.output(parking6green, GPIO.LOW)
-
-
-# turn off red leds
-def turn_off_parking1red():
-    GPIO.output(parking1red, GPIO.LOW)
-
-
-def turn_off_parking2red():
-    GPIO.output(parking2red, GPIO.LOW)
-
-
-def turn_off_parking3red():
-    GPIO.output(parking3red, GPIO.LOW)
-
-
-def turn_off_parking4red():
-    GPIO.output(parking4red, GPIO.LOW)
-
-
-def turn_off_parking5red():
-    GPIO.output(parking5red, GPIO.LOW)
-
-
-def turn_off_parking6red():
-    GPIO.output(parking6red, GPIO.LOW)
-
-
-if parking1_detected:
-    turn_off_parking1red()
-    time.sleep(0.5)
-    turn_on_parking1green()
-    time.sleep(0.5)
-
-else:
-    turn_off_parking1green()
-    time.sleep(0.5)
-    turn_on_parking1red()
-    time.sleep(0.5)
-
-if parking2_detected:
-    turn_off_parking2red()
-    time.sleep(0.5)
-    turn_on_parking2green()
-    time.sleep(0.5)
-
-else:
-    turn_off_parking2green()
-    time.sleep(0.5)
-    turn_on_parking2red()
-    time.sleep(0.5)
-
-if parking3_detected:
-    turn_off_parking3red()
-    time.sleep(0.5)
-    turn_on_parking3green()
-    time.sleep(0.5)
-
-else:
-    turn_off_parking3green()
-    time.sleep(0.5)
-    turn_on_parking3red()
-    time.sleep(0.5)
-
-if parking4_detected:
-    turn_off_parking4red()
-    time.sleep(0.5)
-    turn_on_parking4green()
-    time.sleep(0.5)
-
-else:
-    turn_off_parking4green()
-    time.sleep(0.5)
-    turn_on_parking4red()
-    time.sleep(0.5)
-
-if parking5_detected:
-    turn_off_parking5red()
-    time.sleep(0.5)
-    turn_on_parking5green()
-    time.sleep(0.5)
-
-else:
-    turn_off_parking5green()
-    time.sleep(0.5)
-    turn_on_parking5red()
-    time.sleep(0.5)
-
-if parking6_detected:
-    turn_off_parking6red()
-    time.sleep(0.5)
-    turn_on_parking6green()
-    time.sleep(0.5)
-
-else:
-    turn_off_parking6green()
-    time.sleep(0.5)
-    turn_on_parking6red()
-    time.sleep(0.5)
