ed851654e22a5288f1752aefdf724e1afa17355c
Robin Martens <robin.martens1@student.kuleuven.be>
2022-11-30 17:04:14 +0100

    Fix servo control

--- src/entrance_system/servo_control.py
+++ src/entrance_system/servo_control.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import main1
+from time import sleep
 
 logger = main1.get_logger("servo_control")
 
@@ -9,19 +10,18 @@ def setup_servo(servo_pin: int, pulse_frequency: int):
     logger.info(f"Setup of servo on pin {servo_pin} completed.")
     servo = GPIO.PWM(servo_pin, pulse_frequency)
     servo.start(0)
+    servo.ChangeDutyCycle(7)
+    sleep(1)
     return servo
 
 
-def _set_angle(angle: int, servo, servo_no: int) -> None:
-    duty = angle / 18 + 2
-    servo.ChangeDutyCycle(duty)
-
-
 def open_barrier(servo, servo_no: int) -> None:
-    _set_angle(90, servo, servo_no)
+    servo.ChangeDutyCycle(12)
+    sleep(1)
     logger.info(f"Opened barrier of servo {servo_no}.")
 
 
 def close_barrier(servo, servo_no: int) -> None:
-    _set_angle(0, servo, servo_no)
+    servo.ChangeDutyCycle(7)
+    sleep(1)
     logger.info(f"Closed barrier of servo {servo_no}.")
