6600c5424bbcc00367543781d494115cedc08f66
Robin Martens <robin.martens1@student.kuleuven.be>
2022-11-30 15:34:26 +0100

    Fix servo bug

--- src/entrance_system/main1.py
+++ src/entrance_system/main1.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 import logging
-import sys
 
 #################
 # Logger config #
--- src/entrance_system/servo_control.py
+++ src/entrance_system/servo_control.py
@@ -7,13 +7,14 @@ logger = main1.get_logger("servo_control")
 def setup_servo(servo_pin: int, pulse_frequency: int):
     GPIO.setup(servo_pin, GPIO.OUT)
     logger.info(f"Setup of servo on pin {servo_pin} completed.")
-    return GPIO.PWM(servo_pin, pulse_frequency)
+    servo = GPIO.PWM(servo_pin, pulse_frequency)
+    servo.start(2.5)
+    return servo
 
 
 def _set_angle(angle: int, servo, servo_no: int) -> None:
     duty = angle / 18 + 2
     servo.ChangeDutyCycle(duty)
-    logger.info(f"Set servo {servo_no} to {angle} degrees.")
 
 
 def open_barrier(servo, servo_no: int) -> None:
