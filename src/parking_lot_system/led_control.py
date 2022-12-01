3f982f69f28a96fc7df598bda4b0ece03b79e110
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-01 19:43:33 +0100

    Remove redundant loggers

--- src/parking_lot_system/led_control.py
+++ src/parking_lot_system/led_control.py
@@ -6,11 +6,12 @@ logger = main.get_logger("led_control")
 
 def setup_led(pin_no: int) -> None:
     GPIO.setup(pin_no, GPIO.OUT)
+    GPIO.output(pin_no, GPIO.LOW)
     logger.info(f"Setup of LEDs on pin {pin_no} completed.")
 
 
 def turn_on_green(pin_no: int, parking_no: int) -> None:
-    GPIO.output(pin_no, GPIO.HIGH)
+    GPIO.output(pin_no, GPIO.LOW)
     logger.info(f"Turned on green LED in parking {parking_no}")
 
 
