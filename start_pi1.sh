f6208dc643ef77f3d9b5d18b8c8d6fbf6d4ed1f7
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-06 16:34:33 +0100

    Alter file path

--- start_pi1.sh
+++ start_pi1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Start entrance system..."
-python src/entrance_system/main_pi1.py &
+python ~/Raspberry_Pi/src/entrance_system/main_pi1.py &
 
 echo "Start parking lot system..."
-python src/parking_lot_sytem/main_pi1.py &
+python ~/Raspberry_Pi/src/parking_lot_sytem/main_pi1.py &
 
 echo "Start 7 segment display system..."
-python src/parking_lot_system/7_segment_display_control.py
+python ~/Raspberry_Pi/src/parking_lot_system/7_segment_display_control.py
 
 echo "Startup completed. Opening log..."
 tail -f ~/Raspberry_Pi/rpi_garage.log
