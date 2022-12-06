b3becd67cdae6a3d4b8162e76a46b052316e8205
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-06 20:24:19 +0100

    Add more info to LCD screen

--- src/parking_lot_system/7_segment_display_control.py
+++ src/parking_lot_system/7_segment_display_control.py
@@ -6,6 +6,7 @@ import requests
 import json
 import os
 
+from typing import Any
 from logger import get_logger
 from PIL import ImageFont
 
@@ -26,6 +27,24 @@ RST = 25
 SPI_PORT = 0
 SPI_DEVICE = 0
 
+###################
+# Type definition #
+###################
+class GarageInfo:
+    def __init__(self, name: str, total_spots: int, left_spots: int) -> None:
+        self.name = name
+        self.total_spots = total_spots
+        self.left_spots = left_spots
+
+    @classmethod
+    def fromJSON(cls, json: dict[str, Any]) -> "GarageInfo":
+        return GarageInfo(
+            json["data"]["name"],
+            json["data"]["parkingLots"],
+            json["data"]["unoccupiedLots"],
+        )
+
+
 ##################
 # Main functions #
 ##################
@@ -42,29 +61,35 @@ def setup_display() -> TFT.ST7735:
     return disp
 
 
-def write(inst: TFT.ST7735, string: str) -> None:
+def write(inst: TFT.ST7735, string: str, *, x: int, y: int) -> None:
     font = ImageFont.load_default(size=15)
-    inst.text((10, 10), string, font=font, fill=(255, 255, 255))
+    inst.text((x, y), string, font=font, fill=(255, 255, 255))
 
 
 def reset(inst: TFT.ST7735) -> None:
     inst.reset()
 
 
-def get_free_spots() -> int:
+def get_free_spots() -> GarageInfo:
     url = f"https://po3backend.ddns.net/api/garage/{GARAGE_ID}"
     headers = {"PO3-ORIGIN": "rpi", "PO3-RPI-KEY": os.environ["RPI_KEY"]}
     response = json.loads(requests.get(url, headers=headers).text)
-    return int(response["data"]["unoccupiedLots"])
+    return GarageInfo.fromJSON(response)
 
 
 if __name__ == "__main__":
     disp = setup_display()
     try:
         while True:
-            left_spots = get_free_spots()
-            print(left_spots)
-            write(disp, f"{left_spots}/6")
+            garage_info = get_free_spots()
+            write(disp, garage_info.name, x=10, y=30)
+            write(
+                disp,
+                f"Free spots left: {garage_info.left_spots}/{garage_info.total_spots}",
+                x=10,
+                y=10,
+            )
+            logger.info("Written output to screen.")
             time.sleep(5)
     except KeyboardInterrupt:
         GPIO.cleanup()
