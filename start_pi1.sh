ac7bb5d93e2d1ebfbf5b8867953dbe7cbfd3fc62
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-06 16:37:40 +0100

    Remove capitalization

--- start_pi1.sh
+++ start_pi1.sh
@@ -4,10 +4,10 @@ echo "Start entrance system..."
 python ~/Raspberry_Pi/src/entrance_system/main_pi1.py &
 
 echo "Start parking lot system..."
-python ~/Raspberry_Pi/src/parking_lot_sytem/main_pi1.py &
+python ~/Raspberry_pi/src/parking_lot_sytem/main_pi1.py &
 
 echo "Start 7 segment display system..."
-python ~/Raspberry_Pi/src/parking_lot_system/7_segment_display_control.py
+python ~/Raspberry_pi/src/parking_lot_system/7_segment_display_control.py
 
 echo "Startup completed. Opening log..."
-tail -f ~/Raspberry_Pi/rpi_garage.log
+tail -f ~/Raspberry_pi/rpi_garage.log
