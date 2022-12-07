8cef7db7d276416fccf77310228f55bed92f4bde
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-07 15:10:55 +0100

    Remove image file

--- src/entrance_system/take_image.sh
+++ src/entrance_system/take_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 libcamera-still --immediate -o $1 -n --verbose=0 --hflip --vflip
-curl -F "file=@$1" -H 'charset:UTF8;' -H 'PO3-ORIGIN: rpi' -H "PO3-RPI-KEY: $RPI_KEY" -H "PO3-GARAGE-ID: 11" "https://po3backend.ddns.net/api/images" 
+curl --silent -F "file=@$1" -H 'charset:UTF8;' -H 'PO3-ORIGIN: rpi' -H "PO3-RPI-KEY: $RPI_KEY" -H "PO3-GARAGE-ID: 11" "https://po3backend.ddns.net/api/images" 
+rm $1
