dd432128a8449a93229e429d16b4cb1966586df4
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-07 09:41:04 +0100

    Suppress milliseconds in logs

--- src/entrance_system/logger.py
+++ src/entrance_system/logger.py
@@ -6,6 +6,10 @@ import logging
 def get_logger(name: str) -> logging.Logger:
     log_format = "%(asctime)s: %(message)s (%(name)8s)"
     logging.basicConfig(
-        level=logging.INFO, format=log_format, filename="rpi_garage.log", filemode="w"
+        level=logging.INFO,
+        format=log_format,
+        filename="rpi_garage.log",
+        filemode="w",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
     return logging.getLogger(name)
