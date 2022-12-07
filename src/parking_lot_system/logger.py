04151a91a2bd1c44e9b51f825e289a4dad8ad1aa
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-07 09:57:35 +0100

    Alter logger definition

--- src/parking_lot_system/logger.py
+++ src/parking_lot_system/logger.py
@@ -1,5 +1,29 @@
 import logging
 
+from logging import Logger, RootLogger, WARNING
+
+root = RootLogger(WARNING)
+
+
+class RPiLogger(Logger):
+    def __init__(self, *args) -> None:
+        super().__init__(*args)
+
+    def info(self, msg: object, *args, **kwargs) -> None:
+        return super().info(msg, *args, **kwargs)
+
+
+def getLogger(name=None) -> RPiLogger:
+    """
+    Return a logger with the specified name, creating it if necessary.
+
+    If no name is specified, return the root logger.
+    """
+    if not name or isinstance(name, str) and name == root.name:
+        return root
+    return RPiLogger.manager.getLogger(name)
+
+
 #################
 # Logger config #
 #################
@@ -12,4 +36,4 @@ def get_logger(name: str) -> logging.Logger:
         filemode="w",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    return logging.getLogger(name)
+    return getLogger(name)
