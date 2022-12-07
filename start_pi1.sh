ca2d051a354098b965369538960e94a285679b6f
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-07 10:41:11 +0100

    Add PID output to start script

--- start_pi1.sh
+++ start_pi1.sh
@@ -2,13 +2,14 @@
 
 echo "Start entrance system..."
 python src/entrance_system/main_pi1.py &
-echo "Entrance system running with PID $$"
 sleep 1
+echo "Entrance system running with PID `pgrep -f src/entrance_system/main_pi1.py`"
+
 
 echo "Start parking lot system..."
 python src/parking_lot_system/main_pi1.py &
-echo "Parking lot system running with PID $$"
 sleep 1
+echo "Parking lot system running with PID `pgrep -f src/parking_lot_system/main_pi1.py`"
 
 echo "Startup completed. Opening log..."
 sleep 1
