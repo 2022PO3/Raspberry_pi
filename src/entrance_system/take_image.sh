226f4db818aab0695b5883091ae431769d5981cf
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-13 15:43:51 +0100

    Fix argument bug

--- src/entrance_system/take_image.sh
+++ src/entrance_system/take_image.sh
@@ -6,7 +6,7 @@ curl --silent -H 'charset:UTF8;' -H 'PO3-ORIGIN: rpi' -H "PO3-RPI-KEY: $RPI_KEY"
 libcamera-still --immediate -o $1 -n --verbose=0 --hflip --vflip
 
 # Send the image to the Google Vision API and parse the response with a licence plate regex.
-LP=`gcloud ml vision detect-text $IMAGE_PATH | jq '.responses[].textAnnotations[0].description' | sed -e 's/[[:punct:]]//g' | grep -oE '[[:digit:]][A-Z]{3}[[:digit:]]{3}'`
+LP=`gcloud ml vision detect-text $1 | jq '.responses[].textAnnotations[0].description' | sed -e 's/[[:punct:]]//g' | grep -oE '[[:digit:]][A-Z]{3}[[:digit:]]{3}'`
 # Send the licence plate string to the Backend.
 curl --silent -H 'charset:UTF8;' -H 'PO3-ORIGIN: rpi' -H "PO3-RPI-KEY: $RPI_KEY" -H "PO3-GARAGE-ID: 11" -d "{\"garageId\": 11, \"licencePlate\": \"$LP\"}" "https://po3backend.ddns.net/api/images"
 
