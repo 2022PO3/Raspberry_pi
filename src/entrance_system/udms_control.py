956b03dcb114aebe38b7e599d7cc0fb0fa96a550
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-21 10:06:50 +0100

    Remove print

--- src/entrance_system/udms_control.py
+++ src/entrance_system/udms_control.py
@@ -89,7 +89,6 @@ def run_enter_detection(
         resp = send_licence_plate(licence_plate_system, GARAGE_ID)
         if can_enter(resp):
             servo_state = open_barrier(servo, servo_state, system=system)
-            log("Car can enter the parking", logger)
         return not sensor_state, servo_state
     elif distance >= 10 and sensor_state:
         log(f"Car left {system} sensor.", logger)
